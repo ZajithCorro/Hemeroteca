f062b3f25d29057aa582625d4debf48463e455a8
Zajith Corro <zaja_9508@hotmail.com>
2018-03-13 23:03:00 -0600

    Cambios en diseño
    Cambios menores en diseño de menú, contenido y titulos.
    Agregando mixin a header

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,22 @@ const pug = require('gulp-pug');
 const paths = {
   srcHTML: './source/pug/pages/*.pug',
   srcJS: './source/js/*.js',
-  srcCSS: './source/css/*.css',
 
   tmpHTML: './source/pug/**/*.pug',
   tmpJS: './source/js/**/*.js',
-  tmpCSS: './source/css/**/*.css'
+  tmpCSS: './source/css/*.css'
 };
+
+const srcCSS = [
+  './source/css/generales.css', './source/css/menu.css'
+];
 
 gulp.task('html', () =>
   gulp.src(paths.srcHTML)
     .pipe(plumber())
     .pipe(pug({
       pretty: true
-    }))
+    })) //pretty: true
     .pipe(gulp.dest('./build'))
 );
 
@@ -32,20 +35,20 @@ gulp.task('js', () =>
 );
 
 gulp.task('css', () =>
-  gulp.src(paths.srcCSS)
+  gulp.src(srcCSS)
   .pipe(concat('style.css'))
   .pipe(gulp.dest('./build/css'))
 );
 
 gulp.task('miniCss', () =>
-  gulp.src(paths.srcCSS)
+  gulp.src(srcCSS)
   .pipe(concat('style.min.css'))
   .pipe(minifycss())
   .pipe(gulp.dest('./build/css'))
 );
 
-gulp.task('default', () => {
+gulp.task('default', ['html','css','js','miniCss'], () => {
   gulp.watch(paths.tmpHTML, ['html'])
   gulp.watch(paths.tmpJS, ['js'])
-  gulp.watch(paths.tmpCSS, ['css', 'miniCss'])
+  gulp.watch(paths.tmpCSS, ['css','miniCss'])
 });
